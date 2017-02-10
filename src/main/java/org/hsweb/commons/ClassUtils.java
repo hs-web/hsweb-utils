53dba9513072729c0dd58a80500c824a4b0d85a5
zhouhao <zh.sqy@qq.com>
2017-02-10 15:35:30 +0800

    修复泛型问题

--- src/main/java/org/hsweb/commons/ClassUtils.java
+++ src/main/java/org/hsweb/commons/ClassUtils.java
@@ -4,9 +4,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class ClassUtils {
 
@@ -59,24 +57,11 @@ public class ClassUtils {
         return ann;
     }
 
-    /**
-     * 获取一个类的泛型类型,如果未获取到返回Object.class
-     *
-     * @param clazz 要获取的类
-     * @param index 泛型索引
-     * @return 泛型
-     */
-    public static Class<?> getGenericType(Class clazz, int index) {
-        Type genType = clazz.getGenericSuperclass();
-        if (!(genType instanceof ParameterizedType)) {
-            if (clazz != Object.class) {
-                return getGenericType(clazz.getSuperclass());
-            }
-            return Object.class;
-        }
-        Type[] params = ((ParameterizedType) genType).getActualTypeArguments();
+
+    public static Class<?> getGenericTypeByType(ParameterizedType genType, int index) {
+        Type[] params = genType.getActualTypeArguments();
         if (index >= params.length || index < 0) {
-            throw new RuntimeException("Index outof bounds");
+            return null;
         }
         Object res = params[index];
         if (res instanceof Class) {
@@ -88,6 +73,30 @@ public class ClassUtils {
         return null;
     }
 
+    /**
+     * 获取一个类的泛型类型,如果未获取到返回Object.class
+     *
+     * @param clazz 要获取的类
+     * @param index 泛型索引
+     * @return 泛型
+     */
+    public static Class<?> getGenericType(Class clazz, int index) {
+        List<Type> arrys = new ArrayList<>();
+        arrys.add(clazz.getGenericSuperclass());
+        arrys.addAll(Arrays.asList(clazz.getGenericInterfaces()));
+        return arrys.stream()
+                .filter(Objects::nonNull)
+                .map(type -> {
+                    if (clazz != Object.class && !(type instanceof ParameterizedType)) {
+                        return getGenericType(clazz.getSuperclass(), index);
+                    }
+                    return getGenericTypeByType(((ParameterizedType) type), index);
+                })
+                .filter(Objects::nonNull)
+                .filter(res -> res != Object.class)
+                .findFirst()
+                .orElse((Class) Object.class);
+    }
     /**
      * 获取一个类的第一个泛型的类型
      *
